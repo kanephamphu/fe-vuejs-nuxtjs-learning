d1d7ca0c493455c1864da2fd20521d611b84d5aa
tangquocbao <tangquoc.bao@hitachids.com>
2026-01-23 10:55:33 +0700

    update register user

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -1,3 +1,31 @@
+<style scoped>
+.overlay {
+  position: fixed;
+  inset: 0;
+  background: rgba(0,0,0,0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+form{
+  color: black;
+}
+
+input{
+  color: black;
+  border: 1px solid black;
+}
+
+.dialog {
+  background: #fff;
+  padding: 20px;
+  width: 300px;
+  height: auto;
+  border-radius: 6px;
+}
+</style>
+
 <script setup>
 definePageMeta({
   layout: 'admin'
@@ -17,17 +45,75 @@ const prevPage = () => {
     window.scrollTo(0,0);
   }
 };
+
+
+import { reactive, ref } from 'vue'
+import { useUserStore } from '~/stores/user';
+
+const userStore = useUserStore();
+
+const showDlgNew = ref(false)
+
+const form = reactive({
+  email: '',
+  password: ''
+})
+
+const openDlgNew = () => {
+  showDlgNew.value = true
+}
+
+const closeDlgNew = () => {
+  showDlgNew.value = false
+}
+
+const createAccount = async () => {
+  console.log('Create account:', { ...form })
+  alert(`Account created for ${form.username} ${form.email}`)
+  try{
+    const { error } = await $fetch('/api/auth/register', {
+        method: 'POST',
+        body: {
+          email: form.email,
+          password: form.password
+        }
+      })
+      userStore.setUser(form.username);
+      if (error) throw error
+
+      alert('Tạo account thành công')
+
+      // reset form
+      form.username = ''
+      form.email = ''
+      form.password = ''
+
+      closeDlgNew()
+      refresh()
+  }catch (err) {
+    console.group('CREATE ACCOUNT ERROR')
+    console.log('status:', err.statusCode)
+    console.log('message:', err.data?.message)
+    console.log('full error:', err)
+    console.groupEnd()
+
+    alert(err.data?.message || 'Tạo account thất bại')
+  }
+}
 </script>
 
 <template>
   <div>
     <div class="flex justify-between items-center mb-8">
-      <h1 class="text-3xl font-bold">User Management</h1>
+      <h1 class="text-3xl font-bold">User Management Bao</h1>
       <button @click="refresh" class="px-4 py-2 bg-gray-700 hover:bg-gray-600 rounded-lg text-sm transition">
         🔄 Refresh
       </button>
+      <button @click="openDlgNew" class="px-4 py-2 bg-gray-700 hover:bg-gray-600 rounded-lg text-sm transition">        
+        New User
+      </button>
     </div>
-
+    
     <!-- User Table -->
     <div class="bg-gray-800 rounded-xl border border-gray-700 overflow-hidden shadow-lg">
       <table class="w-full text-left border-collapse">
@@ -87,6 +173,40 @@ const prevPage = () => {
           <button @click="nextPage" class="px-3 py-1 bg-gray-700 rounded text-sm hover:bg-gray-600 transition">Next</button>
         </div>
       </div>
+    </div>
+  </div>
+
+  <!-- Dialog Add new -->
+  <div v-if="showDlgNew" class="overlay">
+    <div class="dialog">
+      <h3 class="text-black">Create account</h3>
+
+      <form @submit.prevent="createAccount">
+        <table cellspacing="2" cellpadding="2" border="1">
+          <tr>
+            <td><label>Username:</label></td>
+            <td><input type="text" name="username" v-model="form.username"/></td>
+          </tr>
+          <tr>
+            <td><label>Email:</label></td>
+            <td><input type="email" name="email" v-model="form.email"/></td>       
+          </tr>   
+          <tr>
+            <td><label>Password:</label></td>
+            <td><input type="password" name="password" v-model="form.password"/></td>       
+          </tr>
+          <tr>
+            <td colspan="2" class="text-right">
+              <button type="button" @click="closeDlgNew" class="mr-4 px-4 py-2 bg-gray-700 text-white hover:bg-gray-600 rounded-lg text-sm transition">
+                Cancel
+              </button>
+              <button type="submit" class="px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg text-sm text-white transition">
+                Create
+              </button>
+            </td>
+          </tr>
+        </table>
+      </form>
     </div>
   </div>
 </template>
